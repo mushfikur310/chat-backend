b0b7b20f8355794e7e6005f7a834c796fc072170
Mushfik <khanmushfikur@gmail.com>
2019-06-19 12:17:42 +0530

    new

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,28 +1,27 @@
-import socket from 'socket.io';
-import http from 'http';
-import Rooms from '../providers/rooms';
+import socket from "socket.io";
+import http from "http";
+import Rooms from "../providers/rooms";
 
 const Room = new Rooms();
 
-const ioEvents = (io)=>{
-    io.on('connection',socket=>{
-        socket.on("SEND", async(data)=> {
-            if(data.room !==''){
-                if(await Room.OnGetSingleRoom(data.room,data.user,socket.id)){
-                    io.emit("RECEIVE", data);
-                }
-            }
-        });
-    })
-}
+const ioEvents = io => {
+  io.on("connection", socket => {
+    socket.on("SEND", async data => {
+      if (data.room !== "") {
+        if (await Room.OnGetSingleRoom(data.room, data.author, socket.id)) {
+          io.emit("RECEIVE", data);
+        }
+      }
+    });
+  });
+};
 
-const socketServer = (app)=>{
-    let server = http.Server(app);
-    let io = socket(server);
+const socketServer = app => {
+  let server = http.Server(app);
+  let io = socket(server);
 
-    ioEvents(io);
-    return server;
-}
+  ioEvents(io);
+  return server;
+};
 
-
-export {socketServer};
+export { socketServer };
